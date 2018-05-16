b0eaae62176be3b283f12f159a36306daea6e11a
jyh <yonghoon.ji@systrangroup.com>
2018-05-16 15:40:06 +0900

    add-feature swagger2

--- api-gateway-webflux/src/main/java/com/example/controller/EgController.java
+++ api-gateway-webflux/src/main/java/com/example/controller/EgController.java
@@ -1,16 +1,17 @@
 package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.info.User;
+
+import io.swagger.annotations.ApiOperation;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class EgController {
-	
+	@ApiOperation(value = "", notes = "Gets user (json)", response = User.class)
 	@GetMapping("/user")
 	public Mono<ResponseEntity<User>> getUser() {
 		return this.getFromRepository().map( s -> ResponseEntity.ok(s)).defaultIfEmpty(ResponseEntity.notFound().build());
--- api-gateway-webflux/src/main/java/com/example/info/User.java
+++ api-gateway-webflux/src/main/java/com/example/info/User.java
@@ -1,9 +1,12 @@
 package com.example.info;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Value;
 
 @Value
 public class User {
+	@ApiModelProperty(required = true, notes = "user name")
 	private String name;
+	@ApiModelProperty(required = true, notes = "user email")
 	private String email;
 }
